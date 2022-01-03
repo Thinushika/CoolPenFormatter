5829c9342018b74b816976a8664215f93074070e
Thinushi <thinupavani@gmail.com>
2022-01-03 17:25:43 +0530

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AssignmentPenFormat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.prograssBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Items.AddRange(new object[] {
+            "D",
+            "E",
+            "F",
+            "G",
+            "I",
+            "J"});
+            this.cmbPort.Location = new System.Drawing.Point(80, 22);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 24);
+            this.cmbPort.TabIndex = 1;
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(126, 98);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(75, 26);
+            this.btnFormat.TabIndex = 2;
+            this.btnFormat.Text = "Format";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // prograssBar
+            // 
+            this.prograssBar.Location = new System.Drawing.Point(80, 68);
+            this.prograssBar.Name = "prograssBar";
+            this.prograssBar.Size = new System.Drawing.Size(121, 12);
+            this.prograssBar.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 152);
+            this.Controls.Add(this.prograssBar);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Format";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.ProgressBar prograssBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentPenFormat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            prograssBar.Value = 10;
+            insertFiles(); // dummy files creating method
+        }
+        private void insertFiles()
+        {
+            bool insertFiles = true; //boolean value for run
+            char disk = cmbPort.SelectedItem.ToString()[0]; //get selected drive
+            try
+            {
+                prograssBar.Value = 15; // this is for progras bar, not nessesory
+                int i = 0;
+                            while (insertFiles)
+                            {
+                                string path = @""+disk+ ":\\myFile"+i+".txt"; //location and name of file that you want to create
+                                using (FileStream fs= new FileStream(path, FileMode.OpenOrCreate)) //this is the function for creating file series
+                                {
+                                    fs.SetLength(1024*1024); //size of the file
+                                    using (TextWriter tw = new StreamWriter(fs)) //function for some txt that you want t write in the file
+                                    {
+                                        tw.WriteLine("hellow"); //txt inside the file
+                                    }
+                                }
+                                    i++; //incrementing by one count
+
+                            }
+                prograssBar.Value = 50;// prograss bar
+            }
+            catch(Exception e)
+            {
+                insertFiles = false;//it become false if boolean value false
+            }
+            finally
+            {
+                prograssBar.Value = 60;// prograss bar
+                FormatDrive_CommandLine(disk); // after creating dummy files, by this method it will format 
+                prograssBar.Value = 100;// prograss bar
+                MessageBox.Show("Complete");//show after creating dummy files
+                this.Close();//end of the function
+            }
+
+        }
+        public static bool FormatDrive_CommandLine(char driveLetter, string label = "", string fileSystem = "NTFS", bool quickFormat = true, bool enableCompression = false, int? clusterSize = null)
+        {
+            #region args check
+
+            if (!Char.IsLetter(driveLetter) ||
+                !IsFileSystemValid(fileSystem))
+            {
+                return false;
+            }
+
+            #endregion//code for Format 
+            bool success = false;
+            string drive = driveLetter + ":";//getting driver letter as a string
+            try
+            {
+                var di = new DriveInfo(drive);
+                var psi = new ProcessStartInfo();
+                psi.FileName = "format.com";
+                psi.CreateNoWindow = true; //if you want to hide the window
+                psi.WorkingDirectory = Environment.SystemDirectory;
+                psi.Arguments = "/FS:" + fileSystem +
+                                             " /Y" +
+                                             " /V:" + label +
+                                             (quickFormat ? " /Q" : "") +
+                                             ((fileSystem == "NTFS" && enableCompression) ? " /C" : "") +
+                                             (clusterSize.HasValue ? " /A:" + clusterSize.Value : "") +
+                                             " " + drive;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardInput = true;
+                var formatProcess = Process.Start(psi);
+                var swStandardInput = formatProcess.StandardInput;
+                swStandardInput.WriteLine();
+                formatProcess.WaitForExit();
+                success = true;
+            }
+            catch (Exception) { }
+            return success;
+        }
+
+        private static bool IsFileSystemValid(string fileSystem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void test()
+        {
+            try
+            {
+                int j = 5000, m;
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+                foreach (DriveInfo d in allDrives)
+                {
+                    if (d.IsReady == true)
+                    {
+                        for (long i = 0; i <= 131072; i++)
+                        {
+                            try
+                            {
+
+                                using (TextWriter writer = File.CreateText(@"" + comboBox1.Text + " " + i + "dummy.txt"))
+                                {
+                                   
+                                    for (m = 0; j >= m; m++)
+                                    {
+                                        writer.WriteLine("Hi " + m);
+
+                                    }
+
+                                }
+
+                            }
+                            catch 
+                            {
+
+                            }
+
+                        }
+                    }
+
+
+                }
+            }
+            catch { }
+        }
+    }
+}
+    }
+}
